abb12e44105cd5426cb63ad10c241b216a483738
Umar yousaf <umaryo@lsretail.com>
2024-12-12 12:57:22 +0800

    [#61171] Fixed discount creation

--- Plugin/Replication/Cron/DiscountCreateTaskPlugin.php
+++ Plugin/Replication/Cron/DiscountCreateTaskPlugin.php
@@ -4,11 +4,10 @@ namespace Ls\Commerce\Plugin\Replication\Cron;
 
 use Exception;
 use \Ls\Core\Model\LSR;
-use \Ls\Omni\Client\Ecommerce\Entity\Enum\ReplDiscountType;
 use \Ls\Replication\Logger\Logger;
-use \Ls\Replication\Model\ResourceModel\ReplDiscountSetup\CollectionFactory;
 use Magento\CatalogRule\Model\ResourceModel\Rule\CollectionFactory as RuleCollectionFactory;
 use Magento\CatalogRule\Model\RuleFactory;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\Stdlib\DateTime\DateTime;
 use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
 use Magento\CatalogRuleStaging\Model\Rule\Retriever;
@@ -17,20 +16,11 @@ use Magento\Staging\Api\UpdateRepositoryInterface;
 use Magento\CatalogRule\Api\Data\RuleInterfaceFactory;
 use Magento\CatalogRuleStaging\Api\CatalogRuleStagingInterface;
 use Magento\Staging\Model\EntityStaging;
-use Magento\Staging\Model\Entity\HydratorInterface;
 use Magento\CatalogRule\Model\ResourceModel\Rule as ResourceRule;
 use Magento\Staging\Model\VersionManager;
-use Magento\Store\Api\Data\StoreConfigInterface;
-use Magento\Store\Model\ScopeInterface;
 
 class DiscountCreateTaskPlugin
 {
-
-    /**
-     * @var CollectionFactory
-     */
-    public $replDiscountCollection;
-
     /**
      * @var UpdateFactory
      */
@@ -96,15 +86,8 @@ class DiscountCreateTaskPlugin
     public $lsr;
 
     /**
-     * @var StoreConfigInterface
-     */
-    public $storeConfig;
-
-    /**
-     * @param CollectionFactory $replDiscountCollection
      * @param UpdateFactory $updateFactory
      * @param UpdateRepositoryInterface $updateRepository
-     * @param RuleCollectionFactory $ruleCollectionFactory
      * @param CatalogRuleStagingInterface $catalogRuleStaging
      * @param ResourceRule $resourceRule
      * @param RuleFactory $ruleFactory
@@ -114,14 +97,11 @@ class DiscountCreateTaskPlugin
      * @param EntityStaging $entityStaging
      * @param Retriever $entityRetriever
      * @param LSR $lsr
-     * @param StoreConfigInterface $storeConfig
      * @param Logger $logger
      */
     public function __construct(
-        CollectionFactory $replDiscountCollection,
         UpdateFactory $updateFactory,
         UpdateRepositoryInterface $updateRepository,
-        RuleCollectionFactory $ruleCollectionFactory,
         CatalogRuleStagingInterface $catalogRuleStaging,
         ResourceRule $resourceRule,
         RuleFactory $ruleFactory,
@@ -131,13 +111,10 @@ class DiscountCreateTaskPlugin
         EntityStaging $entityStaging,
         Retriever $entityRetriever,
         LSR $lsr,
-        StoreConfigInterface $storeConfig,
         Logger $logger
     ) {
-        $this->replDiscountCollection = $replDiscountCollection;
         $this->updateFactory          = $updateFactory;
         $this->updateRepository       = $updateRepository;
-        $this->ruleCollectionFactory  = $ruleCollectionFactory;
         $this->catalogRuleStaging     = $catalogRuleStaging;
         $this->resourceRule           = $resourceRule;
         $this->ruleFactory            = $ruleFactory;
@@ -148,33 +125,43 @@ class DiscountCreateTaskPlugin
         $this->entityStaging          = $entityStaging;
         $this->entityRetriever        = $entityRetriever;
         $this->lsr                    = $lsr;
-        $this->storeConfig            = $storeConfig;
     }
 
     /**
-     * After save plugin
+     * After plugin to create schedule update
      *
      * @param $subject
      * @param $result
+     * @param $rule
      * @param $replValidation
      * @return mixed
+     * @throws \DateInvalidTimeZoneException
      * @throws \DateMalformedStringException
+     * @throws NoSuchEntityException
      */
-    public function afterSave(
+    public function afterSaveCatalogRuleBasedOnDiscountValidation(
         $subject,
         $result,
+        $rule,
         $replValidation
     ) {
-        if ($replValidation->getStartTime() == null || $replValidation->getProcessed() == 0) {
-            return $result;
-        }
-        $navId          = $replValidation->getNavId();
-        $scopeId        = $replValidation->getScopeId();
-        $discountOffers = $this->getDiscountsByValidationPeriod($navId, $scopeId);
+        return $this->updateValidationDateTime(
+            $rule,
+            $replValidation
+        );
+    }
 
-        foreach ($discountOffers as $discountOffer) {
-            $this->updateValidationDateTime($discountOffer->getOfferNo(), $replValidation);
-        }
+    /**
+     * After plugin to add filter to ruleCollection
+     *
+     * @param $subject
+     * @param $result
+     * @return mixed
+     */
+    public function afterGetCatalogRuleCollection($subject, $result)
+    {
+        $result->addFieldToFilter('created_in', ['eq' => 1]);
+        $result->getSelect()->setPart('disable_staging_preview',true); //To prevent staging data in collection
 
         return $result;
     }
@@ -182,136 +169,92 @@ class DiscountCreateTaskPlugin
     /**
      * Update staging
      *
-     * @param $name
+     * @param $rule
      * @param $replValidation
-     * @return void
+     * @return boolean
+     * @throws \DateInvalidTimeZoneException
      * @throws \DateMalformedStringException
+     * @throws NoSuchEntityException
      */
-    public function updateValidationDateTime($name, $replValidation)
+    public function updateValidationDateTime($rule, $replValidation)
     {
-        $ruleCollection = $this->ruleCollectionFactory->create();
-        $ruleCollection->addFieldToFilter('name', ['like' => $name . '%']);
-        $ruleCollection->addFieldToFilter('created_in', ['eq' => 1]);
-        $ruleCollection->getSelect()->setPart('disable_staging_preview',true); //To prevent staging data in collection
+        $ruleId = $rule->getId();
+        $ruleObj  = $this->ruleFactory->create();
+        $this->resourceRule->load($ruleObj, $ruleId);
+        $ruleObj->setIsActive(false);
+        $ruleObj->save();
 
-        if ($ruleCollection->getSize() > 0) {
-            foreach ($ruleCollection as $rule) {
-                $ruleId = $rule->getId();
-                $ruleObj  = $this->ruleFactory->create();
-                $this->resourceRule->load($ruleObj, $ruleId);
-                $ruleObj->setIsActive(false);
-                $ruleObj->save();
-                
-                $activationUpdate = $this->updateFactory->create();
+        $activationUpdate = $this->updateFactory->create();
 
-                $startTime = $replValidation->getStartDate() . " " . $replValidation->getStartTime();
-                //convert start time from central timezone to UTC timezone
-                // to check start time is past current time or not.
-                $startTimeObj           = $this->convertTimezone($startTime);
-                $startTimeUTC           = $startTimeObj->format('Y-m-d h:i:s A');
-                $startTimeInUtcTimezone = $startTimeObj->getTimestamp();
+        $startTime = $replValidation->getStartDate() . " " . $replValidation->getStartTime();
+        //convert start time from central timezone to UTC timezone
+        // to check start time is past current time or not.
+        $startTimeObj           = $this->convertTimezone($startTime);
+        $startTimeUTC           = $startTimeObj->format('Y-m-d h:i:s A');
+        $startTimeInUtcTimezone = $startTimeObj->getTimestamp();
 
-                //Get Current UTC Timestamp
-                $currentDateTime     = new \DateTime();
-                $currentUtcTimeStamp = $currentDateTime->getTimestamp();
+        //Get Current UTC Timestamp
+        $currentDateTime     = new \DateTime();
+        $currentUtcTimeStamp = $currentDateTime->getTimestamp();
 
-                if ($startTimeInUtcTimezone <= $currentUtcTimeStamp) {
-                    //If startTime is a past time, adding 1 minute buffer from current timestamp
-                    //for offer to start.
-                    $startTimeStamp = $currentUtcTimeStamp + (1 * 60);
-                    $startTimeUTC   = $this->dateTime->date('Y-m-d h:i:s A', $startTimeStamp);
-                }
+        if ($startTimeInUtcTimezone <= $currentUtcTimeStamp) {
+            //If startTime is a past time, adding 1 minute buffer from current timestamp
+            //for offer to start.
+            $startTimeStamp = $currentUtcTimeStamp + (1 * 60);
+            $startTimeUTC   = $this->dateTime->date('Y-m-d h:i:s A', $startTimeStamp);
+        }
 
-                $startTime = $this->timezone->date(
-                    new \DateTime($startTimeUTC),
-                    null,
-                    false,
-                    true
-                )->format('Y-m-d\TH:i:s\Z');
+        $startTime = $this->timezone->date(
+            new \DateTime($startTimeUTC),
+            null,
+            false,
+            true
+        )->format('Y-m-d\TH:i:s\Z');
 
-                $activationUpdate->setName($rule->getName())
-                    ->setStartTime(
-                        $startTime
-                    )->setIsCampaign(false);
-                if ($replValidation->getEndTime()) {
-                    $endTime    = $replValidation->getEndDate() . " " . $replValidation->getEndTime();
-                    $endTimeObj = $this->convertTimezone($endTime);
-                    $endTimeUTC = $endTimeObj->format('Y-m-d h:i:s A');
-                    $endTime    = $this->timezone->date(
-                        new \DateTime($endTimeUTC),
-                        null,
-                        false,
-                        true
-                    )->format('Y-m-d\TH:i:s\Z');
+        $activationUpdate->setName($rule->getName())
+            ->setStartTime(
+                $startTime
+            )->setIsCampaign(false);
+        if ($replValidation->getEndTime()) {
+            $endTime    = $replValidation->getEndDate() . " " . $replValidation->getEndTime();
+            $endTimeObj = $this->convertTimezone($endTime);
+            $endTimeUTC = $endTimeObj->format('Y-m-d h:i:s A');
+            $endTime    = $this->timezone->date(
+                new \DateTime($endTimeUTC),
+                null,
+                false,
+                true
+            )->format('Y-m-d\TH:i:s\Z');
 
-                    $activationUpdate->setEndTime($endTime);
-                }
-                try {
-                    //if not the default value, schedule already exists.
-                    //Unschedule the existing one to update new schedule.
-                    if ($rule->getUpdatedIn() != "2147483647") {
-                        $updateId = $rule->getUpdatedIn();
-                        $this->versionManager->setCurrentVersionId($updateId);
-                        $entity = $this->entityRetriever->getEntity($ruleId);
-                        $this->entityStaging->unschedule($entity, $this->versionManager->getVersion()->getId());
-                    }
-
-                    $activationUpdate = $this->updateRepository->save($activationUpdate);
-                    $this->versionManager->setCurrentVersionId($activationUpdate->getId());
-                    $model  = $this->ruleFactory->create();
-                    $this->resourceRule->load($model, $ruleId);
-                    $model->setIsActive(true);
-                    $this->catalogRuleStaging->schedule($model, $activationUpdate->getId());
-                } catch (Exception $e) {
-                    $this->logger->debug($e->getMessage());
-                }
-
+            $activationUpdate->setEndTime($endTime);
+        }
+        try {
+            //if not the default value, schedule already exists.
+            //Unschedule the existing one to update new schedule.
+            if ($rule->getUpdatedIn() != "2147483647") {
+                $updateId = $rule->getUpdatedIn();
+                $this->versionManager->setCurrentVersionId($updateId);
+                $entity = $this->entityRetriever->getEntity($ruleId);
+                $this->entityStaging->unschedule($entity, $this->versionManager->getVersion()->getId());
             }
+
+            $activationUpdate = $this->updateRepository->save($activationUpdate);
+            $this->versionManager->setCurrentVersionId($activationUpdate->getId());
+            $model  = $this->ruleFactory->create();
+            $this->resourceRule->load($model, $ruleId);
+            $model->setIsActive(true);
+            $this->catalogRuleStaging->schedule($model, $activationUpdate->getId());
+            return true;
+        } catch (Exception $e) {
+            $this->logger->debug($e->getMessage());
         }
-    }
 
-    /**
-     * Fetch offers based on validation period
-     *
-     * @param $navId
-     * @param $scopeId
-     * @return array|\Ls\Replication\Model\ResourceModel\ReplDiscountSetup\Collection
-     */
-    public function getDiscountsByValidationPeriod($navId, $scopeId)
-    {
-        $publishedOfferIds = [];
-        $collection        = $this->replDiscountCollection->create();
-        $collection->addFieldToFilter('scope_id', $scopeId);
-        $collection->addFieldToFilter('ValidationPeriodId', $navId);
-        $collection->getSelect()
-            ->columns(['OfferNo', 'LineType', 'LineNumber'])
-            ->group('OfferNo');
-
-        $collection->addFieldToFilter(
-            'Type',
-            ReplDiscountType::DISC_OFFER
-        );
-
-        $collection->addFieldToFilter(
-            'Enabled',
-            1
-        );
-
-        $collection->addFieldToFilter(
-            'IsDeleted',
-            0
-        );
-
-        $query = $collection->getSelect()->__toString();
-        if ($collection->getSize() > 0) {
-            return $collection;
-        }
-        return $publishedOfferIds;
+        return false;
     }
 
     /**
      * @return array|string
-     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     * @throws NoSuchEntityException
      */
     public function getCentralTimeZone()
     {
@@ -330,7 +273,7 @@ class DiscountCreateTaskPlugin
      * @return string
      * @throws \DateInvalidTimeZoneException
      * @throws \DateMalformedStringException
-     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     * @throws NoSuchEntityException
      */
     public function convertTimezone($datetime, $fromTimezone = null, $toTimezone = null)
     {
